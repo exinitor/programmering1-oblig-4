9e373bfadf3980627782953bec6a37c3301322b7
Jan Einar Kristensen <jan_e_k@hotmail.com>
2016-04-11 19:54:55 +0200

    h

--- ObligatoriskeOppgaverV16.4/src/SnuTekst.java
+++ ObligatoriskeOppgaverV16.4/src/SnuTekst.java
@@ -8,21 +8,20 @@ public class SnuTekst {
 	 this.tekst = tekst;
  	}
  
-	public String getTekst() {
-		return tekst;
-	}
-
-	public void setTekst(String tekst) {
-		this.tekst = tekst;
-	}
-
 	public static void baklengs(String tekst){
 		System.out.print(tekst.charAt(tekst.length()-1));
-		
+		if(tekst.length() > 1)
+		{
+			baklengs(tekst.substring(0,tekst.length()), antallForandringer);
+		}
+	
+	}
+	public static void baklengs(String tekst, int siste) {	
 		if(tekst.length() > 1)
 		{
 				baklengs(tekst.substring(0,tekst.length()-1));
 				antallForandringer++;
 		}
+		
 	}
 }
